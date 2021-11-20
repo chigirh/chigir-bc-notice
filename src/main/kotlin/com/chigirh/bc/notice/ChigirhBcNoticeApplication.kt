1f8d6dae08b11c4bd19ebd44e7e8bf86de8750e6
chigirh <abch9431@gmail.com>
2021-11-20 19:23:01 +0900

    first commit

---
+++ src/main/kotlin/com/chigirh/bc/notice/ChigirhBcNoticeApplication.kt
@@ -0,0 +1,11 @@
+package com.chigirh.bc.notice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChigirhBcNoticeApplication
+
+fun main(args: Array<String>) {
+	runApplication<ChigirhBcNoticeApplication>(*args)
+}
